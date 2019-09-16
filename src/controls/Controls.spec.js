46d565a6a5192e45e263ed80fe7b015ec34d520b
Jordan <jordandoan@hotmail.com>
2019-09-16 11:22:09 -0700

    Finished testing for disabled button when locked and default text

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+
+import React from 'react';
+import { render } from 'react-testing-library'
+import Controls from './Controls';
+
+
+test('contains text for default state: unlocked and open', () => {
+  const { queryByText } = render(<Controls />);
+  expect(queryByText(/close/i)).toBeTruthy();
+});
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
 // Test away
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library'
+import Dashboard from './Dashboard';
+
+test('contains text for default state: unlocked and open', () => {
+  const {getByText} = render(<Dashboard />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+  getByText(/lock gate/i);
+  getByText(/close gate/i);
+});
+
+
+test('open/close gate is disabled when gate is locked', () => {
+  const {getByText} = render(<Dashboard />);
+  fireEvent.click(getByText(/close gate/i));
+  fireEvent.click(getByText(/lock gate/i));
+  expect(getByText(/open gate/i).disabled).toBeTruthy();
+});
